e25c1bb89136e99d2ae61bcdde810a98d97053c0
Randy Eustache <eustachet@gmail.com>
2016-07-14 13:33:04 +0000

    Changed new action in profiles controller

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -2,8 +2,7 @@ class ProfilesController < ApplicationController
     def new
         # form where a user can fill out their own profile.
         @user = User.find( params[:user_id] )
-        @profile = @user.build_profile
-    end
+        @profile = Profile.new
     
     def create 
         @user = User.find( params[:user_id] )
@@ -20,4 +19,4 @@ class ProfilesController < ApplicationController
     def profile_params
       params.require(:profile).permit(:first_name, :last_name, :job_title, :phone_number, :contact_email, :description)
     end
-end
+  end
